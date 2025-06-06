dfc4681f7bb1dfd3693ccdf9118153bbdfdd2fb9
WindowsMEMZ <memz1@darock.top>
2025-06-06 17:47:07 +0800

    Minor changes

--- Package.swift
+++ Package.swift
@@ -6,11 +6,17 @@ import PackageDescription
 let package = Package(
     name: "MarkdownUI",
     products: [
-        .library(name: "MarkdownUI", targets: ["MarkdownUI"]),
+        .library(name: "MarkdownUI", targets: ["MarkdownUI", "_MarkdownUIDependenciesWrapper"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Darock-Studio/DarockUI", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "MarkdownUI", path: "MarkdownUI.xcframework")
+        .binaryTarget(name: "MarkdownUI", path: "MarkdownUI.xcframework"),
+        .target(name: "_MarkdownUIDependenciesWrapper", dependencies: [
+            "DarockUI"
+        ])
     ]
 )
